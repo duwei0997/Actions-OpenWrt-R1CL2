ceb2aecb376fd3814a8ac2e25785210c6365a72a
duwei0997 <duwei0997@gmail.com>
2026-08-21 17:43:52 +0800

    Update default IP and hostname in diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+# hostname
+sed -i 's/OpenWrt/R1CL/g' package/base-files/files/bin/config_generate
+# wifiname
+sed -i 's/OpenWrt/R1CL/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
